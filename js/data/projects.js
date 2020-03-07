b969b3f63847b7576854b453354db428b8075fba
Ting Wang <itingwang35@gmail.com>
2020-03-07 13:02:43 -0500

    update links and add resume

--- js/data/projects.js
+++ js/data/projects.js
@@ -32,16 +32,7 @@ var projectsData = {
       "img/modal/pnc_7.png"
     ],
     "captions":[
-      "Transforming raw unintelligible data into actionable, visual presentations to engage stakeholders",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      ""
-    ]
-
+      "Transforming raw unintelligible data into actionable, visual presentations to engage stakeholders", "", "", "", "", "", "", ""]
   },{
     "name": "City of Pittsburgh Utility App",
     "desc": "user interface mockup for city utility data",
@@ -59,27 +50,73 @@ var projectsData = {
 
     "captions": ["", "", "", "", "", "", "", ""]
   },{
-    "name": "Poster",
-    "desc": "posters for School of Architecture events",
-    "skillTag": ["Illustrator"],
-    "thumb": "img/thumb/poster.svg",
-    "images": [
-      "img/modal/poster_1.png",
-      "img/modal/poster_2.png"
-    ],
-    "captions": ["", ""]
+    "name": "Scrumptious Stops",
+    "desc": "Google Maps & Foursquare API trip planner",
+    "skillTag": ["Illustrator", "JavaScript", "HTML", "CSS", "Git"],
+    "thumb": "img/thumb/scrumptious.svg",
+    "images": ["img/modal/scrumptious_1.PNG", "img/modal/scrumptious_2.PNG"],
+    "captions": [
+      "An app to map my favorite restaurants and cafes in central Florida. <a href='https://itingw.github.io/Scrumptious-Stops/' target='_blank'>See it live! <i class='fas fa-external-link-alt'></i></a> ",
+      "Capability to highlight favorite food and cafes near routes <a href='https://github.com/itingw/Scrumptious-Stops/' target='_blank'> Code <i class='fas fa-external-link-alt'></i></a>"
+    ]
   },{
-    "name": "Hand Drawing",
-    "desc": "perspective drawing, architectural diagrams, anatomy",
-    "skillTag": ["Sketching", "Photoshop"],
-    "thumb": "img/thumb/drawings.svg",
-    "images": [
-      "img/modal/draw_1.jpg",
-      "img/modal/draw_2.jpg",
-      "img/modal/draw_3.jpg",
-      "img/modal/draw_4.jpg"
-    ],
-    "captions": ["", "", "", "",]
+    "name": "Bug Invasion",
+    "desc": "revamped classic arcade game",
+    "skillTag": ["JavaScript", "Git"],
+    "thumb": "img/thumb/bug.svg",
+    "images": ["img/modal/bug_1.PNG", "img/modal/bug_2.PNG"],
+    "captions": [
+      "Revamped classic arcade game instructions <a href='https://itingw.github.io/Bug-Invasion/' target='_blank'>Play here! <i class='fas fa-external-link-alt'></i></a>",
+      "Ten possible maps with increasing enemy bugs as the player progresses<a href='https://github.com/itingw/Bug-Invasion' target='_blank'> Code <i class='fas fa-external-link-alt'></i></a>"
+    ]
+  },{
+    "name": "Address Book",
+    "desc": "master detail user interface with CRUD",
+    "skillTag": ["Illustrator", "JavaScript", "HTML", "CSS", "Git"],
+    "thumb": "img/thumb/addressbook.svg",
+    "images": ["img/modal/addressbook_1.PNG","img/modal/addressbook_2.PNG"],
+    "captions": [
+      "A simple address book featuring feline friends demonstrating CRUD operations in a master-detail interface. <a href='https://itingw.github.io/address-book/' target='_blank'>See it live! <i class='fas fa-external-link-alt'></i></a>",
+      "Updating a user's address book entry <a href='https://github.com/itingw/address-book' target='_blank'>Code <i class='fas fa-external-link-alt'></i></a>"
+    ]
+  },{
+    "name": "RunTrack App",
+    "desc": "UI design for running tracking application",
+    "skillTag": ["XD","Illustrator"],
+    "thumb": "img/thumb/run.svg",
+    "images": ["img/modal/run_1.png","img/modal/run_2.png","img/modal/run_3.png","img/modal/run_4.png"],
+    "captions": [
+      "A <a href='https://www.dailyui.co/' target='_blank'> daily UI challenge design </a><i class='fas fa-external-link-alt'></i> for a running app",
+      "", "", ""]
+  },{
+    "name": "Chickpea Yarns",
+    "desc": "UI design for potential commercial application",
+    "skillTag": ["XD","Illustrator"],
+    "thumb": "img/thumb/chickpea.svg",
+    "images": ["img/modal/yarn_1.png","img/modal/yarn_2.png","img/modal/yarn_3.png","img/modal/yarn_4.png"],
+    "captions": [
+        "A <a href='https://www.dailyui.co/' target='_blank'> daily UI challenge design </a> for a product page",
+        "Color swatch selection UI",
+        "Product page in mobile screen sizes",
+        "Shopping cart - check out the <a href='https://www.etsy.com/shop/chickpeayarns' target='_blank'><i class='fas fa-external-link-alt'></i> Etsy store </a>"
+      ]
+  },{
+    "name": "Rock Climbing App",
+    "desc": "UI design for rock climbing recording application",
+    "skillTag": ["XD","Illustrator"],
+    "thumb": "img/thumb/climbing.svg",
+    "images": ["img/modal/climb_1.png","img/modal/climb_2.png"],
+    "captions": [
+      "User workflow for a rock climbing journal.",
+      "Workout workflow design for <a href='https://www.dailyui.co/' target='_blank'> Daily UI challenge design </a><i class='fas fa-external-link-alt'></i>"
+    ]
+  },{
+    "name": "Portfolio",
+    "desc": "the making of",
+    "skillTag": ["Illustrator", "XD", "JavaScript", "HTML", "CSS", "Git", "Sketching"],
+    "thumb": "img/thumb/portfolio.svg",
+    "images": ["img/modal/portfolio_4.jpg", "img/modal/portfolio_1.PNG", "img/modal/portfolio_2.PNG", "img/modal/portfolio_3.PNG",],
+    "captions": ["idea wireframing for portfolio", "project prototyping proposal", "project prototyping proposal", "inspirations and references. <a href='https://github.com/itingw/portfolio/' target='_blank'>Code <i class='fas fa-external-link-alt'></i></a>"]
   },{
     "name": "Architectural Design",
     "desc": "sustainable design, design-build",
@@ -96,73 +133,26 @@ var projectsData = {
     ],
     "captions": ["", "", "", "", "", "", ""]
   },{
-    "name": "Scrumptious Stops",
-    "desc": "Google Maps & Foursquare API trip planner",
-    "skillTag": ["Illustrator", "JavaScript", "HTML", "CSS", "Git"],
-    "thumb": "img/thumb/scrumptious.svg",
-    "images": ["img/modal/scrumptious_1.png", "img/modal/scrumptious_2.png"],
-    "captions": [
-      "An app to map my favorite restaurants and cafes in central Florida. <a href='https://itingw.github.io/Scrumptious-Stops/'>See it live! <i class='fas fa-external-link-alt'></i></a> ",
-      "Capability to highlight favorite food and cafes near routes <a href='https://github.com/itingw/Scrumptious-Stops/'> Code <i class='fas fa-external-link-alt'></i></a>"
-    ]
+    "name": "Hand Drawing",
+    "desc": "perspective drawing, architectural diagrams, anatomy",
+    "skillTag": ["Sketching", "Photoshop"],
+    "thumb": "img/thumb/drawings.svg",
+    "images": [
+      "img/modal/draw_1.jpg",
+      "img/modal/draw_2.jpg",
+      "img/modal/draw_3.jpg",
+      "img/modal/draw_4.jpg"
+    ],
+    "captions": ["", "", "", "",]
   },{
-    "name": "Bug Invasion",
-    "desc": "revamped classic arcade game",
-    "skillTag": ["JavaScript", "Git"],
-    "thumb": "img/thumb/bug.svg",
-    "images": ["img/modal/bug_1.png", "img/modal/bug_2.png"],
-    "captions": [
-      "Revamped classic arcade game instructions <a href='https://itingw.github.io/Bug-Invasion/'>Play here! <i class='fas fa-external-link-alt'></i></a>",
-      "Ten possible maps with increasing enemy bugs as the player progresses<a href='https://github.com/itingw/Bug-Invasion'> Code <i class='fas fa-external-link-alt'></i></a>"
-    ]
-  },{
-    "name": "Portfolio",
-    "desc": "the making of",
-    "skillTag": ["Illustrator", "XD", "JavaScript", "HTML", "CSS", "Git", "Sketching"],
-    "thumb": "img/thumb/portfolio.svg",
-    "images": ["img/modal/portfolio_4.jpg", "img/modal/portfolio_1.png", "img/modal/portfolio_2.png", "img/modal/portfolio_3.png",],
-    "captions": ["idea wireframing for portfolio", "project prototyping proposal", "project prototyping proposal", "inspirations and references"]
-  },{
-    "name": "Address Book",
-    "desc": "master detail user interface with CRUD",
-    "skillTag": ["Illustrator", "JavaScript", "HTML", "CSS", "Git"],
-    "thumb": "img/thumb/addressbook.svg",
-    "images": ["img/modal/addressbook_1.png","img/modal/addressbook_2.png"],
-    "captions": [
-      "A simple address book featuring feline friends demonstrating CRUD operations in a master-detail interface. <a href='https://itingw.github.io/address-book/'>See it live! <i class='fas fa-external-link-alt'></i></a>",
-      "Updating a user's address book entry <a href='https://github.com/itingw/address-book'>Code <i class='fas fa-external-link-alt'></i></a>"
-    ]
-  },{
-    "name": "RunTrack App",
-    "desc": "UI design for running tracking application",
-    "skillTag": ["XD","Illustrator"],
-    "thumb": "img/thumb/run.svg",
-    "images": ["img/modal/run_1.png","img/modal/run_2.png","img/modal/run_3.png","img/modal/run_4.png"],
-    "captions": [
-      "A <a href='https://www.dailyui.co/'> daily UI challenge design </a><i class='fas fa-external-link-alt'></i> for a running app",
-      "", "", ""]
-  },{
-    "name": "Chickpea Yarns",
-    "desc": "UI design for potential commercial application",
-    "skillTag": ["XD","Illustrator"],
-    "thumb": "img/thumb/chickpea.svg",
-    "images": ["img/modal/yarn_1.png","img/modal/yarn_2.png","img/modal/yarn_3.png","img/modal/yarn_4.png"],
-    "captions": [
-        "A <a href='https://www.dailyui.co/'> daily UI challenge design </a> for a product page",
-        "Color swatch selection UI",
-        "Product page in mobile screen sizes",
-        "Shopping cart - check out the <a href='https://www.etsy.com/shop/chickpeayarns'><i class='fas fa-external-link-alt'></i> Etsy store </a>"
-      ]
-  },{
-    "name": "Rock Climbing App",
-    "desc": "UI design for rock climbing recording application",
-    "skillTag": ["XD","Illustrator"],
-    "thumb": "img/thumb/climbing.svg",
-    "images": ["img/modal/climb_1.png","img/modal/climb_2.png"],
-    "captions": [
-      "User workflow for a rock climbing journal.",
-      "Workout workflow design for <a href='https://www.dailyui.co/'> Daily UI challenge design </a><i class='fas fa-external-link-alt'></i>"
-
-    ]
+    "name": "Poster",
+    "desc": "posters for School of Architecture events",
+    "skillTag": ["Illustrator"],
+    "thumb": "img/thumb/poster.svg",
+    "images": [
+      "img/modal/poster_1.png",
+      "img/modal/poster_2.png"
+    ],
+    "captions": ["", ""]
   }]
 };
